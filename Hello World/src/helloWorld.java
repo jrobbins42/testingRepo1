e351f12360c3d2cb27282dd0bd66a65bd788be3f
James Robbins <jrobbins051@gmail.com>
2026-03-08 16:12:17 -0500

    commit

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -3,5 +3,8 @@ public class helloWorld{
 	//Justin Ridout
 	public static void main(String[] args) {
 		System.out.println("Hello Word");
+		//James Robbins
+				String name = "your name here";
+				System.out.println(name);
 	}
 }
